4c9b863426b610b1095560b5d7f6e6ca77c51681
Wei Xiao <92381786+Weixy21@users.noreply.github.com>
2024-08-27 16:06:57 -0400

    Update train.py

--- e2e_driving/train.py
+++ e2e_driving/train.py
@@ -17,20 +17,21 @@ from torch.nn.functional import normalize
 torch.autograd.set_detect_anomaly(True)
 
 parser = argparse.ArgumentParser('ODE demo')
-parser.add_argument('--method', type=str, choices=['dopri5', 'adams'], default='dopri5')
-parser.add_argument('--data_size', type=int, default=100)
+parser.add_argument('--method', type=str, choices=['dopri8', 'adams'], default='dopri8')   #dopri5
+parser.add_argument('--activation', type=str, choices=['gelu', 'silu', 'tanh'], default='gelu') 
+parser.add_argument('--data_size', type=int, default=100)   # length of each trajectory
 parser.add_argument('--batch_time', type=int, default=10)
-parser.add_argument('--batch_size', type=int, default=20)
-parser.add_argument('--niters', type=int, default=2000)
-parser.add_argument('--test_freq', type=int, default=20)
+parser.add_argument('--batch_size', type=int, default=20) 
+parser.add_argument('--niters', type=int, default=200)  #2000
+parser.add_argument('--test_freq', type=int, default=2)  #20
 parser.add_argument('--viz', action='store_true')
 parser.add_argument('--gpu', type=int, default=0)
 parser.add_argument('--adjoint', action='store_true')
 parser.add_argument('--consider_noise', action='store_true')
-parser.add_argument('--num_var', type=int, default=1)
+parser.add_argument('--num_var', type=int, default=1)  # number of parameters in the neural ODE chosen (to be modified) to enforce the invariance
 parser.add_argument('--use_cnn', action='store_true')
 parser.add_argument('--use_fc', action='store_true')
-parser.add_argument('--use_diff_weight', action='store_true')
+parser.add_argument('--use_diff_weight', action='store_true')  # for loss
 
 args = parser.parse_args()
 
@@ -39,13 +40,15 @@ if args.adjoint:
 else:
     from torchdiffeq import odeint
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 device = torch.device('cuda:' + str(args.gpu) if torch.cuda.is_available() else 'cpu')
+
+
 exp_id = '0'  
+
 t = torch.linspace(0., 9.9, args.data_size).to(device)
 
-
-pkl_file = open('data3.pkl','rb')
+pkl_file = open('./data/data4.pkl','rb')
 data = pickle.load(pkl_file)
 lidar = torch.tensor(data['lidar']).float()  # batch, seq, data
 control = torch.tensor(data['ctrl']).float()
@@ -53,22 +56,24 @@ ego = torch.tensor(data['ego']).float()
 other = torch.tensor(data['other']).float()
 pkl_file.close()
 
+# Training data
 lidar_bat = lidar.unsqueeze(2).to(device)   # batch, seq, data_h x data_w
 control_bat = control.unsqueeze(2).to(device)
 ego_bat = ego.unsqueeze(2).to(device)
 other_bat = other.unsqueeze(2).to(device)
+lidar_bat = lidar_bat/200.  #normalize lidar
+ego_bat[:,:,:,3] = ego_bat[:,:,:,3]/180 # normalize speed
 
-#test data
-lidar = lidar_bat[-1,:,:,:]
-control = control_bat[-1,:,:,:]
-ego = ego_bat[-1,:,:,:]
-other = other_bat[-1,:,:,:]
+#Testing data
+lidar_test = lidar_bat[-1,:,:,:]
+control_test = control_bat[-1,:,:,:]
+ego_test = ego_bat[-1,:,:,:]
+other_test = other_bat[-1,:,:,:]
 
-control0 = control_bat[-1,0,:,:]
-lidar0 = lidar_bat[-1,0,:,:]
-ego0 = ego_bat[-1,0,:,:]
-
-
+# initial condition for closed-loop testing
+control_test0 = control_bat[-1,0,:,:]
+lidar_test0 = lidar_bat[-1,0,:,:]
+ego_test0 = ego_bat[-1,0,:,:]
 
 def cvx_solver(Q, p, G, h):
     mat_Q = matrix(Q.cpu().numpy())
@@ -81,14 +86,21 @@ def cvx_solver(Q, p, G, h):
     return sol['x']
 
 
-def get_batch():
-    s = torch.from_numpy(np.random.choice(np.arange(args.data_size - args.batch_time, dtype=np.int64), args.batch_size, replace=False))
+def get_batch(bitr):  # get training data from trajectory bitr
+    control = control_bat[bitr,:,:,:]
+    lidar = lidar_bat[bitr,:,:,:]
+    ego = ego_bat[bitr,:,:,:]
+    bb = torch.from_numpy(np.random.choice(np.arange(args.data_size - args.batch_time, dtype=np.int64), args.batch_size-1, replace=False))
+    aa = torch.tensor([0])   # augment 0
+    s = torch.cat([bb, aa], dim = 0)
     batch_u0 = control[s]  # (M, D)
     batch_lidar0 = lidar[s]
+    batch_ego0 = ego[s]
     batch_t = t[:args.batch_time]  # (T)
     batch_u = torch.stack([control[s + i] for i in range(args.batch_time)], dim=0)  # (T, M, D)
     batch_lidar = torch.stack([lidar[s + i] for i in range(args.batch_time)], dim=0)
-    return batch_u0.to(device), batch_lidar0.to(device), batch_t.to(device), batch_u.to(device), batch_lidar.to(device)
+    batch_ego = torch.stack([ego[s + i] for i in range(args.batch_time)], dim=0)
+    return batch_u0.to(device), batch_lidar0.to(device), batch_ego0.to(device), batch_t.to(device), batch_u.to(device), batch_lidar.to(device), batch_ego.to(device)
 
 
 def makedirs(dirname):
@@ -97,7 +109,10 @@ def makedirs(dirname):
 
 
 if args.viz:
-    makedirs('logs/png' + exp_id)
+    if args.consider_noise:
+        makedirs('png_noise')
+    else:
+        makedirs('png_cbf' + exp_id)
     import matplotlib.pyplot as plt
     fig = plt.figure(figsize=(8, 4), facecolor='white')
     ax_traj = fig.add_subplot(121, frameon=False)
@@ -116,6 +131,7 @@ def visualize(true_y, pred_y, itr):
         ax_traj.plot(t.cpu().numpy(), true_y.cpu().numpy()[:, 0, 0], t.cpu().numpy(), true_y.cpu().numpy()[:, 0, 1], 'g-')
         ax_traj.plot(t.cpu().numpy(), pred_y.cpu().numpy()[:, 0, 0], '--', t.cpu().numpy(), pred_y.cpu().numpy()[:, 0, 1], 'b--')
         ax_traj.set_xlim(t.cpu().min(), t.cpu().max())
+        # ax_traj.set_ylim(-2, 2)
         ax_traj.legend()
 
         ax_phase.cla()
@@ -124,10 +140,14 @@ def visualize(true_y, pred_y, itr):
         ax_phase.set_ylabel('u2')
         ax_phase.plot(true_y.cpu().numpy()[:, 0, 0], true_y.cpu().numpy()[:, 0, 1], 'g-')
         ax_phase.plot(pred_y.cpu().numpy()[:, 0, 0], pred_y.cpu().numpy()[:, 0, 1], 'b--')
+        # ax_phase.set_xlim(-2, 2)
+        # ax_phase.set_ylim(-2, 2)
 
         fig.tight_layout()
-
-        plt.savefig('logs/png' + exp_id +'/{:03d}'.format(itr))
+        if args.consider_noise:
+            plt.savefig('png_noise/{:03d}'.format(itr))
+        else:
+            plt.savefig('png_cbf' + exp_id +'/{:03d}'.format(itr))
         plt.draw()
         plt.pause(0.001)
 
@@ -138,20 +158,22 @@ class ODEFunc(nn.Module):
                  dropout= 0.2):
         super(ODEFunc, self).__init__()
 
-        self.net = nn.Sequential(
-            nn.Linear(fc_param[0], fc_param[1]),
-            nn.GELU(), #silu(), gelu()
-            nn.Linear(fc_param[1], fc_param[2]),
-            nn.GELU(),  #Tanh
-            nn.Linear(fc_param[2], fc_param[3])  #drift and affine terms
-        )
-        
+        # self.net = nn.Sequential(
+        #     nn.Linear(fc_param[0], fc_param[1]),
+        #     nn.GELU(), #silu(), gelu()
+        #     nn.Linear(fc_param[1], fc_param[2]),
+        #     nn.GELU(),  #Tanh
+        #     nn.Linear(fc_param[2], fc_param[3])  #drift and affine terms
+        # )
+        self.net = self.build_mlp(fc_param)
+        # import pdb; pdb. set_trace()
         for m in self.net.modules():
             if isinstance(m, nn.Linear):
                 nn.init.normal_(m.weight, mean=0, std=0.1)
                 nn.init.constant_(m.bias, val=0)
         self.use_dcbf = False
-        self.lidar = lidar0
+        self.lidar = lidar_test0
+        self.ego = ego_test0
         # self.pa = Parameter(torch.ones(1))
         # self.pb = Parameter(torch.ones(1))
         if args.use_cnn == True:
@@ -166,24 +188,75 @@ class ODEFunc(nn.Module):
     def forward(self, t, y):
         if self.training:
             if args.use_fc == True:
-                sensor = self._fc(self.lidar)
+                x = self._fc(self.lidar)
             g = self.net(y)
             u1, u2 = torch.chunk(g[:,:,2:], 2, dim=-1)
+            
             if args.use_fc == True:
-                out = torch.cat([(u1*sensor).sum(axis = 2).unsqueeze(2), (u2*sensor).sum(axis = 2).unsqueeze(2)], dim = 2) + g[:,:,0:2]
+                sensor = torch.cat([x, self.ego[:,:,2:4]], dim = 2)  # add theta and speed
             else:
-                out = torch.cat([(u1*self.lidar).sum(axis = 2).unsqueeze(2), (u2*self.lidar).sum(axis = 2).unsqueeze(2)], dim = 2) + g[:,:,0:2] #affine form
+                sensor = torch.cat([self.lidar, self.ego[:,:,2:4]], dim = 2)
+            out = torch.cat([(u1*sensor).sum(axis = 2).unsqueeze(2), (u2*sensor).sum(axis = 2).unsqueeze(2)], dim = 2) + g[:,:,0:2]
         else:
             if args.use_fc == True:
-                sensor = self._fc(self.lidar)
+                x = self._fc(self.lidar)
             g = self.net(y)
             u1, u2 = torch.chunk(g[:,2:], 2, dim=-1)
+            
             if args.use_fc == True:
-                out = torch.cat([(u1*sensor).sum(axis = 1).unsqueeze(1), (u2*sensor).sum(axis = 1).unsqueeze(1)], dim = 1) + g[:,0:2]
+                sensor = torch.cat([x, self.ego[:,2:4]], dim = 1)  # add theta and speed
             else:
-                out = torch.cat([(u1*self.lidar).sum(axis = 1).unsqueeze(1), (u2*self.lidar).sum(axis = 1).unsqueeze(1)], dim = 1) + g[:,0:2]
+                sensor = torch.cat([self.lidar, self.ego[:,2:4]], dim = 1)
+            out = torch.cat([(u1*sensor).sum(axis = 1).unsqueeze(1), (u2*sensor).sum(axis = 1).unsqueeze(1)], dim = 1) + g[:,0:2]
+            
         return out
+
+    def dCBF(self, u, I, f, g1_complete, g2_complete, ego, other): # used in testing, u-output of neural ODE, I-lidar info., ego-ego state, other-other vehicle state, the remainings are CBF constraints related.
+        x, y, theta, v = ego[0,0], ego[0,1], ego[0,2], ego[0,3]*180
+        x0, y0, theta0, v0 = other[0,0], other[0,1], other[0,2], other[0,3]
+        u1, u2 = u[0,0], u[0,1]
+        f1, f2 = f[0,0], f[0,1]
+        cos_theta, sin_theta = torch.cos(theta), torch.sin(theta)
+        r = 15.5
+        g1, g2 = g1_complete[0:-2], g2_complete[0:-2]
+        state = torch.clone(ego) 
+        
+        f1_state, f2_state = (g1_complete[-2:]*state[0,2:4]).sum(axis = 0), (g2_complete[-2:]*state[0,2:4]).sum(axis = 0)
+        dim = g1.shape[0]
     
+        b = (x - x0)**2 + (y - (y0 - 12))**2 - r**2
+   
+        Lfb = 2*(x - x0)*(v*cos_theta - v0) + 2*(y - (y0 - 12))*v*sin_theta
+        Lf2b = 2*(v*cos_theta - v0)**2 + 2*(v*sin_theta)**2 \
+            + 2*(x - x0)*(cos_theta*u2 - v*sin_theta*u1) \
+                + 2*(y - (y0 - 12))*(sin_theta*u2 + v*cos_theta*u1)
+        Lf3b = 6*(v*cos_theta - v0)*(u2*cos_theta - v*sin_theta*u1) \
+            + 2*(x - x0)*(-sin_theta*u1*u2 + cos_theta*(f2 + f2_state) - u2*sin_theta*u1 - v*cos_theta*u1**2 - v*sin_theta*(f1+f1_state)) \
+                + 2*(y - (y0 - 12))*(cos_theta*u1*u2 + sin_theta*(f2 + f2_state) + u2*cos_theta*u1 - v*sin_theta*u1**2 + v*cos_theta*(f1+f1_state))\
+                    + 6*v*sin_theta*(u2*sin_theta + v*cos_theta*u1)
+        LgLfbnu = (2*(x - x0)*(-v*sin_theta) + 2*(y - (y0 - 12))*v*cos_theta)*g1 \
+            + (2*(x - x0)*cos_theta + 2*(y - (y0 - 12))*sin_theta)*g2
+        LgLfbnu = LgLfbnu.unsqueeze(0)
+        
+        k = 2
+        b_safe = Lf3b + 3*k*Lf2b + 3*k**2*Lfb + k**3*b
+        A_safe = -LgLfbnu
+
+        G = A_safe.to(device)
+        h = b_safe.unsqueeze(0).to(device)
+
+        Q = torch.eye(dim).to(device)
+        q = -I.unsqueeze(1)
+
+        x = cvx_solver(Q.double(), q.double(), G.double(), h.double())
+        out = []
+        for i in range(dim):
+            out.append(x[i])
+        out = np.array(out)
+        out = torch.tensor(out).float().to(device)
+        out = out.unsqueeze(0)
+        return out
+
     def _build_cnn(self, filters, dropout=0., no_act_last_layer=False):
         
         modules = nn.ModuleList()
@@ -196,6 +269,26 @@ class ODEFunc(nn.Module):
                     modules.append(nn.Dropout(p=dropout))
         modules = nn.Sequential(*modules)
         
+        return modules
+    
+    def build_mlp(self, filters, dropout=0.2, no_act_last_layer=True, activation='gelu'):
+        if activation == 'gelu':
+            activation = nn.GELU()
+        elif activation == 'silu':
+            activation = nn.SiLU()
+        elif activation == 'tanh':
+            activation = nn.Tanh()
+        else:
+            raise NotImplementedError(f'Not supported activation function {activation}')
+        modules = nn.ModuleList()
+        for i in range(len(filters)-1):
+            modules.append(nn.Linear(filters[i], filters[i+1]))
+            if not (no_act_last_layer and i == len(filters)-2):
+                modules.append(activation)
+                # if dropout > 0.:
+                #     modules.append(nn.Dropout(p=dropout))
+
+        modules = nn.Sequential(*modules)
         return modules
 
 
@@ -221,16 +314,19 @@ class RunningAverageMeter(object):
 if __name__ == '__main__':
 
     ii = 0
-    makedirs('logs/model' + exp_id)
+    if args.consider_noise:
+        makedirs('models_noise')
+    else:
+        makedirs('models_cbf' + exp_id)
     
     if args.use_cnn or args.use_fc:
-        fc_param = [2, 64, 128, 26]  # [2, 32, 64, 26] 
+        fc_param = [2, 64, 128, 30]  # [2, 32, 64, 26]   # +ego speed, ego heading
     else:
-        fc_param = [2, 50, 400, 202]
+        # fc_param = [2, 50, 400, 206]
+        fc_param = [2, 64, 256, 512, 206]
     #channel x output channels x kernel size x stride x padding
     conv_param = [[1, 4, 5, 2, 1], [4, 8, 3, 2, 1], [8, 12, 3, 2, 0]]
     func = ODEFunc(fc_param, conv_param).to(device)
-    func.lidar = lidar0
     
     optimizer = optim.RMSprop(func.parameters(), lr=1e-3)
     end = time.time()
@@ -238,67 +334,74 @@ if __name__ == '__main__':
     time_meter = RunningAverageMeter(0.97)
     
     loss_meter = RunningAverageMeter(0.97)
+    test_loss = torch.tensor([0]).to(device)
     tt = torch.tensor([0., 0.1]).to(device)
     for itr in range(1, args.niters + 1):
-        func.use_dcbf = False
-        func.train()
-        optimizer.zero_grad()
-        batch_u0, batch_lidar0, batch_t, batch_u, batch_lidar = get_batch()
-        u0 = batch_u0
-        pred_u = u0.unsqueeze(0)
-        for i in range(args.batch_time-1):
-            # func.lidar = normalize(batch_lidar[i,:,:,:], p = 2, dim = 2)
-            sensor = batch_lidar[i,:,:,:]/30.
-            if args.use_cnn == True:
-                sensor = func._cnn(sensor)
-                sensor = sensor.max(dim=-1)[0]
-                func.lidar = sensor.unsqueeze(1)
+        for bitr in range(200):  # 200 trajectories
+            func.use_dcbf = False
+            func.train()
+            optimizer.zero_grad()
+            batch_u0, batch_lidar0, batch_ego0, batch_t, batch_u, batch_lidar, batch_ego = get_batch(bitr)
+            u0 = batch_u0
+            pred_u = u0.unsqueeze(0)
+            for i in range(args.batch_time-1):
+                print('iteration: ', itr, '| traj: ', bitr, '| step:', i, 'test loss: ', test_loss.item())
+                batch_lidar_i = batch_lidar[i,:,:,:]
+                if args.use_cnn == True:
+                    sensor_cnn = func._cnn(batch_lidar_i)
+                    sensor_max = sensor_cnn.max(dim=-1)[0]
+                    func.lidar = sensor_max.unsqueeze(1)
+                else:
+                    func.lidar = batch_lidar_i
+                func.ego = batch_ego[i,:,:,:] 
+                pred = odeint(func, u0, tt).to(device)   # , method = 'explicit_adams' euler, midpoint, rk4, explicit_adams, fixed_adams
+                #"dopri8", "dopri5", "bosh3", "fehlberg2", "adaptive_heun", "euler", "midpoint", "rk4", "explicit_adams", "implicit_adams", "fixed_adams", "scipy_solver"
+                pred_u = torch.cat([pred_u, pred[-1,:,:,:].unsqueeze(0)], dim = 0)
+                u0 = pred[-1,:,:,:]
+            
+            if args.use_diff_weight:
+                loss = 0.01*torch.mean(torch.abs(pred_u[:,:,:,0] - batch_u[:,:,:,0])) + 0.99*torch.mean(torch.abs(pred_u[:,:,:,1] - batch_u[:,:,:,1]))
             else:
-                func.lidar = sensor
-            pred = odeint(func, u0, tt).to(device)
-            pred_u = torch.cat([pred_u, pred[-1,:,:,:].unsqueeze(0)], dim = 0)
-            u0 = pred[-1,:,:,:]
-        
-        if args.use_diff_weight:
-            loss = 0.01*torch.mean(torch.abs(pred_u[:,:,:,0] - batch_u[:,:,:,0])) + 0.99*torch.mean(torch.abs(pred_u[:,:,:,1] - batch_u[:,:,:,1]))
-        else:
-            loss = torch.mean(torch.abs(pred_u - batch_u))
-        loss.backward()
-        optimizer.step()
+                loss = torch.mean(torch.abs(pred_u - batch_u))
+            loss.backward()
+            optimizer.step()
 
-        time_meter.update(time.time() - end)
-        loss_meter.update(loss.item())
+            time_meter.update(time.time() - end)
+            loss_meter.update(loss.item())
 
 
         if itr % args.test_freq == 0:
             with torch.no_grad():
                 func.use_dcbf = False
-                u0 = control0
+                u0 = control_test0
                 pred_u = u0.unsqueeze(0)
                 func.eval()
                 for i in range(args.data_size-1):
-                    # func.lidar = normalize(lidar[i,:,:], p = 2, dim = 1)
-                    sensor = lidar[i,:,:]/30.
+                    lidar_test_i = lidar_test[i,:,:]
                     if args.use_cnn == True:
-                        x = sensor.unsqueeze(0)
-                        sensor = func._cnn(x)
-                        func.lidar = sensor.max(dim=-1)[0]
+                        x = lidar_test_i.unsqueeze(0)
+                        sensor_cnn = func._cnn(x)
+                        func.lidar = sensor_cnn.max(dim=-1)[0]
                     else:
-                        func.lidar = sensor
-                    
+                        func.lidar = lidar_test_i
+                    func.ego = ego_test[i,:,:] 
                     pred = odeint(func, u0, tt)
                     pred_u = torch.cat([pred_u, pred[-1,:,:].unsqueeze(0)], dim = 0)
                     u0 = pred[-1,:,:]
                 if args.use_diff_weight:
-                    loss = 0.01*torch.mean(torch.abs(pred_u[:,:,0] - control[:,:,0])) + 0.99*torch.mean(torch.abs(pred_u[:,:,1] - control[:,:,1]))
+                    test_loss = 0.01*torch.mean(torch.abs(pred_u[:,:,0] - control_test[:,:,0])) + 0.99*torch.mean(torch.abs(pred_u[:,:,1] - control_test[:,:,1]))
                 else:
-                    loss = torch.mean(torch.abs(pred_u - control))
+                    test_loss = torch.mean(torch.abs(pred_u - control_test))
 
-                print('Iter {:04d} | Total Loss {:.6f}'.format(itr, loss.item()))
-                visualize(control, pred_u, ii)
+                print('Iter {:04d} | Total Loss {:.6f}'.format(itr, test_loss.item()))
+                visualize(control_test, pred_u, ii)
                 ii += 1
-            torch.save(func.state_dict(), "./logs/model"+ exp_id + "/model_node_itr" + format(ii, '02d') + ".pth")
+            if args.consider_noise:
+                torch.save(func.state_dict(), "./models_noise/model_node_itr" + format(ii, '02d') + ".pth")
+            else:
+                torch.save(func.state_dict(), "./models_cbf"+ exp_id + "/model_node_itr" + format(ii, '02d') + ".pth")
             
         end = time.time()
 
-    
+    # print(torch.cuda.memory_allocated() / torch.cuda.max_memory_allocated())
+    # continue
